10159fef2d64dc4a88277e5b6dc3325d0343dad6
Thomas Guillory <thomasvguillory@gmail.com>
2023-08-26 08:52:32 +0200

    added interpolation mode

--- src/RingBuffer.cpp
+++ src/RingBuffer.cpp
@@ -10,18 +10,49 @@ RingBuffer::RingBuffer(int max_size, int initial_delay){
     std::size_t size = static_cast<std::size_t>  (max_size * m_sampling_frequency);
     m_buffer.resize(size);
     m_buffer_size = static_cast<int>(size);
+    m_buffer_size-- ;
     m_write = initial_delay * m_sampling_frequency;
     m_read = 0;
 }
 
-/// @brief increment read pointer and read sample from buffer
-/// @return 
-float RingBuffer::readSample(){
-float read_sample = 0;
-m_read = m_read == m_buffer_size ? 0 : m_read + 1;
-read_sample = m_buffer[m_read];
-return read_sample;
+/// @brief increment pointer and set its int, incremented int and frac value
+void RingBuffer::incrementReadPointer(){
+
+   m_read += m_step_size;
+   if (m_read > m_buffer_size) m_read -= m_buffer_size;
+   else if (m_read < 0) m_read += m_buffer_size;
+    m_i_read = static_cast<int>(trunc(m_read));
+    m_frac = m_read - static_cast<float>(m_i_read);
+    m_i_read_next = m_i_read + 1 > m_buffer_size ? 0 : m_i_read + 1;
 }
+
+/// @brief increment read pointer and return sample from interpolation
+float RingBuffer::readSample(){
+    incrementReadPointer();
+    if (m_interpolation_mode == 1) noInterpolation();
+    else if (m_interpolation_mode == 2) linearInterpolation();
+    else if (m_interpolation_mode == 3) allpassInterpolation();
+
+return m_output_sample;
+}
+
+void RingBuffer::noInterpolation(){
+    m_output_sample = m_buffer[m_i_read];
+}
+
+/// @brief Interpolation lineaire du buffer a un index flottant donne
+void RingBuffer::linearInterpolation(){
+    // S[n]=frac * Buf[i+1]+(1-frac)*Buf[i]
+    m_output_sample = (m_frac * m_buffer[m_i_read+1]) + ((1-m_frac) * m_buffer[m_i_read]);
+}
+
+
+/// @brief Interpolation passe-tout, recursion
+void RingBuffer::allpassInterpolation(){
+    //S[n]=Buf[i+1]+(1-frac)*Buf[i]-(1-frac)*S[n-1]
+    m_output_sample = (m_buffer[m_i_read+1]) + ((1-m_frac) * m_buffer[m_i_read]) - ((1-m_frac) * m_output_sample);
+}
+
 
 /// @brief increment write pointer and write input sample in buffer
 /// @param input_sample 
--- src/RingBuffer.hpp
+++ src/RingBuffer.hpp
@@ -7,13 +7,25 @@ RingBuffer(int max_size, int initial_delay);
 float readSample();
 void writeSample(float input_sample);
 float interpolateSample();
+void linearInterpolation();
+void noInterpolation();
+void allpassInterpolation();
+void incrementReadPointer();
+
 
 private:
 int m_sampling_frequency = 48000;
 std::vector<float> m_buffer;
 float m_read, m_write;
+int m_i_read;
+int m_i_read_next;
+float m_step_size = 1;
+int m_interpolation_mode = 1;
+//m_buffer_size en base 0
 int m_size_goal, m_actual_size, m_buffer_size;
 float sample;
+float m_frac;
+float m_output_sample;
 
 };
 }
