1ce5523d20ad0416ca5bbe6261e0e5e410d47636
Thomas Guillory <thomasvguillory@gmail.com>
2024-05-09 20:01:14 +0200

    trimmed glsl file to strict minimum

--- src/demoComponent.h
+++ src/demoComponent.h
@@ -1,11 +1,8 @@
 #pragma once
 
-#include "../Assets/DemoUtilities.h"
 
 //==============================================================================
-class OpenGLDemo2D final : public Component,
-                           private CodeDocument::Listener,
-                           private Timer
+class OpenGLDemo2D final : public Component
 {
 public:
     OpenGLDemo2D()
@@ -16,27 +13,6 @@ public:
             peer->setCurrentRenderingEngine (0);
 
         openGLContext.attachTo (*getTopLevelComponent());
-
-        addAndMakeVisible (statusLabel);
-        statusLabel.setJustificationType (Justification::topLeft);
-        statusLabel.setFont (Font (14.0f));
-
-        auto presets = getPresets();
-
-        for (int i = 0; i < presets.size(); ++i)
-            presetBox.addItem (presets[i].name, i + 1);
-
-        addAndMakeVisible (presetLabel);
-        presetLabel.attachToComponent (&presetBox, true);
-
-        addAndMakeVisible (presetBox);
-        presetBox.onChange = [this] { selectPreset (presetBox.getSelectedItemIndex()); };
-
-        fragmentEditorComp.setOpaque (false);
-        fragmentDocument.addListener (this);
-        addAndMakeVisible (fragmentEditorComp);
-
-        presetBox.setSelectedItemIndex (0);
 
         // setSize (500, 500);
     }
@@ -63,7 +39,6 @@ public:
 
                 if (result.failed())
                 {
-                    statusLabel.setText (result.getErrorMessage(), dontSendNotification);
                     shader.reset();
                 }
             }
@@ -71,7 +46,7 @@ public:
 
         if (shader.get() != nullptr)
         {
-            statusLabel.setText ({}, dontSendNotification);
+            // statusLabel.setText ({}, dontSendNotification);
 
             shader->fillRect (g.getInternalContext(), getLocalBounds());
         }
@@ -79,78 +54,25 @@ public:
 
     void resized() override
     {
-        auto area = getLocalBounds().reduced (4);
-
-        statusLabel.setBounds (area.removeFromTop (75));
-
-        area.removeFromTop (area.getHeight() / 2);
-
-        auto presets = area.removeFromTop (25);
-        presets.removeFromLeft (100);
-        presetBox.setBounds (presets.removeFromLeft (150));
-
-        area.removeFromTop (4);
-        fragmentEditorComp.setBounds (area);
     }
 
-    void selectPreset (int preset)
-    {
-        fragmentDocument.replaceAllContent (getPresets()[preset].fragmentShader);
-        startTimer (1);
-    }
 
     std::unique_ptr<OpenGLGraphicsContextCustomShader> shader;
 
-    Label statusLabel, presetLabel  { {}, "Shader Preset:" };
-    ComboBox presetBox;
-    CodeDocument fragmentDocument;
-    CodeEditorComponent fragmentEditorComp  { fragmentDocument, nullptr };
-    String fragmentCode;
+    String fragmentCode = "void main()\n"
+                "{\n"
+                "    " JUCE_MEDIUMP " vec4 colour1 = vec4 (1.0, 0.4, 0.6, 1.0);\n"
+                "    " JUCE_MEDIUMP " vec4 colour2 = vec4 (0.0, 0.8, 0.6, 1.0);\n"
+                "    " JUCE_MEDIUMP " float alpha = pixelPos.x / 1000.0;\n"
+                "    gl_FragColor = pixelAlpha * mix (colour1, colour2, alpha);\n"
+                "}\n";
 
 private:
     OpenGLContext openGLContext;
 
-    enum { shaderLinkDelay = 500 };
 
-    void codeDocumentTextInserted (const String& /*newText*/, int /*insertIndex*/) override
-    {
-        startTimer (shaderLinkDelay);
-    }
-
-    void codeDocumentTextDeleted (int /*startIndex*/, int /*endIndex*/) override
-    {
-        startTimer (shaderLinkDelay);
-    }
-
-    void timerCallback() override
-    {
-        stopTimer();
-        fragmentCode = fragmentDocument.getAllContent();
-        repaint();
-    }
-
-    struct ShaderPreset
-    {
-        const char* name;
-        const char* fragmentShader;
-    };
-
-    static Array<ShaderPreset> getPresets()
-    {
-        #define SHADER_2DDEMO_HEADER \
-            "/*  This demo shows the use of the OpenGLGraphicsContextCustomShader,\n" \
-            "    which allows a 2D area to be filled using a GL shader program.\n" \
-            "\n" \
-            "    Edit the shader program below and it will be \n" \
-            "    recompiled in real-time!\n" \
-            "*/\n\n"
-
-        ShaderPreset presets[] =
-        {
+/*
             {
-                "Simple Gradient",
-
-                SHADER_2DDEMO_HEADER
                 "void main()\n"
                 "{\n"
                 "    " JUCE_MEDIUMP " vec4 colour1 = vec4 (1.0, 0.4, 0.6, 1.0);\n"
@@ -207,9 +129,6 @@ private:
                 "}\n"
             }
         };
-
-        return Array<ShaderPreset> (presets, numElementsInArray (presets));
-    }
-
+*/
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OpenGLDemo2D)
 };
